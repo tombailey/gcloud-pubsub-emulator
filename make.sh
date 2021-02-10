9cc7d6c84b7f764adef3fc976b5a81f18f488a06
Tom <5710046+tombailey@users.noreply.github.com>
2021-02-11 00:52:52 +0900

    Remove bash script for docker image creation

--- make.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker build -t gcloud-pubsub-emulator:latest .
-docker push docker.io/marcelcorso/gcloud-pubsub-emulator:latest
-docker push docker.io/messagebird/gcloud-pubsub-emulator:latest
